1a6809c36e3e5d470550ddb1a1812f3519996a67
Rainer Mueller-Knoche <rainer.mueller-knoche@muekno.de>
2025-11-09 16:22:27 +0100

    schreibfehler

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,8 @@
  * @note neuer branch am 29.10.2025
  * @date weiter 30.10.2025 .h verstanden Umbau
  * @date 01.11.2025 Schnauze voll alles zurück nach main.cpp
- * @date 0ß.11.2025 Branch nach msaster
+ * @date 09.11.2025 Branch nach msaster
+ * 
  */
 
 #include <Arduino.h>
